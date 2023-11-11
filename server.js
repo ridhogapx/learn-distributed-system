35937fbd80a749c1ef64d262fb0a44d8ed90eb17
RageNeko26 <rneko2006@gmail.com>
2023-11-12 00:50:58 +0700

    feat: Applying services in entry point

--- server.js
+++ server.js
@@ -16,6 +16,40 @@ const init = async() => {
     host: 'localhost',
   });
 
+  server.route([
+    {
+      method: 'GET',
+      path: '/{id}',
+      hander: async(req, h) => {
+        const { id } = req.params;
+
+        try { 
+          const [order, user] = await Promise.all([
+            got(`${orderService}/${id}`),
+            got(`${userService/${id}}`),
+          ]);
+
+          return {
+            id: order.id,
+            menu: order.menu,
+            user: user.name,
+          };
+
+        } catch(err) {
+          if(!err.response) throw err;
+          if (err.response.statusCode === 400) {
+            return h.response({ message: 'bad request'}).code(400);
+          }
+          if (err.response.statusCode === 404) {
+            return h.response({ message: 'not found'}).code(404);
+          }
+
+          throw err;
+        }
+      },
+    }
+  ])
+
   await server.start();
   console.log(`Server is running on ${server.info.uri}`);
 }
